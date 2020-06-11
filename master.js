e75a67a07c74833fb1aeb320d9f57bf6e2f42040
chinmay866 <37858624+chinmay866@users.noreply.github.com>
2020-06-11 21:12:49 +0530

    Bingo Version 1.0
    Deployment Version
    
    Dated 11-06-2020

--- master.js
+++ master.js
@@ -1,28 +1,38 @@
 
-var cell = $("table#bingoTable td")
+var cell = $("table#bingoTable td");
+
+var bingo = $("table.bingo td");
 
 var num = 1;
+var box = 0;
 
-function numbers(cellNumber){
-
-
-}
+var a = [];
+var b = [];
+var c = [];
+var d = [];
 
 function reset() {
 
   $(cell).text("")
+  $("#reset").text("Reset")
+  $('.alert').text("Fill in Numbers");
   $(cell).css("background-color", "white")
   $(cell).on("click", function() {
 
     $(this).css("background-color", "white")
     $(this).css("color", "black")
   });
+  $(bingo).css("background-color", "white");
+  $(bingo).css("color", "black");
   num = 1;
+  box = 0;
 }
 
 function start(condition) {
 
   if (condition === 26){
+
+    $("#reset").text("End")
 
     for (var i = 0; i < cell.length; i++){
 
@@ -30,59 +40,96 @@ function start(condition) {
 
         $(this).css("background-color", "black")
         $(this).css("color", "white")
+        winA();
+        winB();
+        winAlert()
       });
     }
   }
 }
 
-function activateStart(){
+function bingoBox() {
 
-  $("#start").removeClass("disabled");
+  if ($(bingo[box]).css("background-color") !== "rgba(0, 0, 0)"){
+
+    $(bingo[box]).css("background-color", "black");
+    $(bingo[box]).css("color", "white");
+    box += 1;
+  }
 }
 
-function win() {
+function winA() {
 
   x = 0;
   y = 0;
-  zA = 0;
-  zB = 0;
 
   while(x < 20){
 
     if ( $(cell[x]).css("background-color") === "rgb(0, 0, 0)" && $(cell[x+1]).css("background-color") === "rgb(0, 0, 0)" && $(cell[x+2]).css("background-color") === "rgb(0, 0, 0)" && $(cell[x+3]).css("background-color") === "rgb(0, 0, 0)" && $(cell[x+4]).css("background-color") === "rgb(0, 0, 0)"){
 
-      console.log("1 Horizontal Line");
+      if (a.indexOf(x) === -1) {
+
+        a.push(x);
+        bingoBox();
+      }
     }
 
     x += 5;
   }
 
-  while(y < 4){
+  while(y < 5){
 
-    if ( $(cell[y]).css("background-color") === "rgb(0, 0, 0)" && $(cell[y+5]).css("background-color") === "rgb(0, 0, 0)" && $(cell[y+10]).css("background-color") === "rgb(0, 0, 0)" && $(cell[y+15]).css("background-color") === "rgb(0, 0, 0)" && $(cell[y+20].css("background-color") === "rgb(0, 0, 0)")){
+    if ( $(cell[y]).css("background-color") === "rgb(0, 0, 0)" && $(cell[y+5]).css("background-color") === "rgb(0, 0, 0)" && $(cell[y+10]).css("background-color") === "rgb(0, 0, 0)" && $(cell[y+15]).css("background-color") === "rgb(0, 0, 0)" && $(cell[y+20]).css("background-color") === "rgb(0, 0, 0)"){
 
-      console.log("1 Vertical Line");
+      if (b.indexOf(y) === -1) {
+
+        b.push(y);
+        bingoBox();
+      }
     }
 
     y += 1;
   }
 
-  while(zA < 2){
+}
 
-    if ( $(cell[zA]).css("background-color") === "rgb(0, 0, 0)" && $(cell[zA+6]).css("background-color") === "rgb(0, 0, 0)" && $(cell[zA+12]).css("background-color") === "rgb(0, 0, 0)" && $(cell[zA+18]).css("background-color") === "rgb(0, 0, 0)" && $(cell[zA+24].css("background-color") === "rgb(0, 0, 0)")){
+function winB() {
 
-      console.log("1A Dioganal Line");
+  x = 0;
+  y = 4;
+
+  if(x === 0){
+
+    if ( $(cell[x]).css("background-color") === "rgb(0, 0, 0)" && $(cell[x+6]).css("background-color") === "rgb(0, 0, 0)" && $(cell[x+12]).css("background-color") === "rgb(0, 0, 0)" && $(cell[x+18]).css("background-color") === "rgb(0, 0, 0)" && $(cell[x+24]).css("background-color") === "rgb(0, 0, 0)"){
+
+      if (c.indexOf(x) === -1) {
+
+        c.push(x);
+        bingoBox();
+      }
     }
   }
 
-  while(zB < 6){
+  if(y === 4){
 
-    if ( $(cell[zB+4]).css("background-color") === "rgb(0, 0, 0)" && $(cell[zB+8]).css("background-color") === "rgb(0, 0, 0)" && $(cell[zB+12]).css("background-color") === "rgb(0, 0, 0)" && $(cell[zB+16]).css("background-color") === "rgb(0, 0, 0)" && $(cell[zB+20].css("background-color") === "rgb(0, 0, 0)")){
+    if ( $(cell[y]).css("background-color") === "rgb(0, 0, 0)" && $(cell[y+8]).css("background-color") === "rgb(0, 0, 0)" && $(cell[y+12]).css("background-color") === "rgb(0, 0, 0)" && $(cell[y+16]).css("background-color") === "rgb(0, 0, 0)" && $(cell[y+20]).css("background-color") === "rgb(0, 0, 0)"){
 
-      console.log("1B Dioganal Line");
+      if (d.indexOf(y) === -1) {
+
+        d.push(y);
+        bingoBox();
+      }
     }
   }
+}
 
+function winAlert(){
+
+  if (box === 5) {
+
+    $('.alert').text("You Won");
+    $("#reset").text("New Game")
+  }
 }
 
 // GAME
@@ -98,20 +145,14 @@ for (var i = 0; i < cell.length; i++) {
       num += 1;
       if (num === 26){
 
-        activateStart();
+        start(num);
+        $('.alert').text("Play")
       }
     }
   });
 }
 
-$("#start").click(function() {
-
-  start(num);
-});
-
 $("#reset").click(function() {
 
   reset();
 });
-
-setInterval("win()", 1000);
